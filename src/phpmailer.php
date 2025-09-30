e897a50d383dfadd3d2811934f2d29114b22fd1c
Jouw Naam <jouw@email.nl>
2025-09-30 10:46:42 +0200

    logger toegevoegt

--- src/phpmailer.php
+++ src/phpmailer.php
@@ -4,6 +4,15 @@
 
     use PHPMailer\PHPMailer\PHPMailer;
     use PHPMailer\PHPMailer\Exception;
+    use Monolog\Level;
+    use Monolog\Logger;
+    use Monolog\Handler\StreamHandler;
+
+    // create a log channel
+
+
+
+
 
     $mijnemail = "jospartijtegendeburgers@gmail.com";
     $onderwerp = "Bevestiging van je klacht";
@@ -51,6 +60,19 @@
             $mail->Subject = 'Bevestiging van je klacht';
             $mail->Body = "Beste $naam,\n\nWe hebben je klacht ontvangen en zullen deze zo snel mogelijk behandelen.\n\nMet vriendelijke groet,\nKlantenservice";
             $mail->send();
+
+            $log = new Logger('mailer');
+           $logFile = '../Log/info.log';
+
+            $log->pushHandler(new StreamHandler($logFile, Level::Info));
+            $log->info('E-mail is verzonden naar ' . $email . $beschrijvingklacht);
+            $log->debug('debug');
+
+            $log->warning('Dit is een warning');
+            $log->error('Dit is een error');
+
+
+
             echo 'Bevestigingsmail is verzonden.';
         } catch (Exception $e) {
             echo "Er is een fout opgetreden: " . $e->getMessage();
